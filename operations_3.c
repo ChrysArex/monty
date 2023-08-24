a062e94e28620305af77c35055fc313f73151a1e
root <root@localhost.localdomain>
2023-08-24 17:05:08 +0100

    error message fixed for each command

--- operations_3.c
+++ operations_3.c
@@ -19,7 +19,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 	}
 	if (n < 2)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	second->n -= (*stack)->n;
@@ -48,7 +48,7 @@ void _div(stack_t **stack, unsigned int line_number)
 	}
 	if (n < 2)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
@@ -81,7 +81,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 	}
 	if (n < 2)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	second->n *= (*stack)->n;
@@ -110,7 +110,7 @@ void _mod(stack_t **stack, unsigned int line_number)
 	}
 	if (n < 2)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
